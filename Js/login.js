e9630c8bc7cef7e09af2155544edc17b5898433c
Ramiro Di Carlo <ramirodicarlo1@gmail.com>
2021-04-04 23:11:07 -0300

    Finished register.js

--- Js/login.js
+++ Js/login.js
@@ -1,4 +1,4 @@
- document.getElementById("validationbutton").onclick = validation;
+ document.getElementById("validation-button").onclick = validation;
 function validation (){
     console.log("Validation run");
     var myLabelObjet = document.getElementsByTagName("label");
@@ -10,14 +10,14 @@ function validation (){
             var myEmail = mEmail.data;
             var comparationE = myEmail == "Email";
             if (comparationE){
-                var compE = "It has an Email label";
+                var compE = "It has an Email label.";
             }
             else{
-                var compE = "It doesn't have an Email label";
+                var compE = "It doesn't have an Email label.";
             }
         } 
         else{
-            var compE = "It doesn't have an Email label";
+            var compE = "It doesn't have an Email label.";
         }
     
         var makeString1 = document.getElementById("password");
@@ -40,42 +40,76 @@ function validation (){
     };
     var myInputObjet = document.getElementsByTagName("input");
     var inputLength = myInputObjet.length;
-
     if(inputLength != 0){
         var makeString2 = document.getElementsByName("email");
         if( makeString2 != null){
             var iEmail = makeString2[0].type;
             var comparationE = iEmail == "email";
             if (comparationE){
-                var inpE = "It has an Email input";
+                var inpE = "It has an Email input.";
             }
             else{
-                var inpE = "It doesn't have an Email input";
+                var inpE = "It doesn't have an Email input.";
             }
         } 
         else{
-            var inpE = "It doesn't have an Email input";
+            var inpE = "It doesn't have an Email input.";
         }
+        if(makeString2[0].required){
+            var inpER = "Email has a required atribute.";
+        }
+        else{
+            var inpER = "Email doesn't have a required atribute."
+        };
     
         var makeString3 = document.getElementsByName("password");
         if( makeString3 != null){
             var iPassword = makeString3[0].type;
             var comparationP = iPassword == "password";
             if (comparationP){
-                var inpP = "It has a Password input";
+                var inpP = "It has a Password input.";
             }
             else{
-                var inpP = "It doesn't have a Password input";
+                var inpP = "It doesn't have a Password input.";
             }
         }
         else{
-            var inpP = "It doesn't have a Password input";
+            var inpP = "It doesn't have a Password input.";
+        }
+        if(makeString3[0].required){
+            var inpPR = "Password has a required atribute.";
+        }
+        else{
+            var inpPR = "Password doesn't have a required atribute."
+        };
+        var makeString4 = document.getElementsByName("login");
+        if( makeString4 != null){
+            var iLogin = makeString4[0].type;
+            var comparationL = iLogin == "submit";
+            if (comparationL){
+                var inpL = "It has a Login input.";
+            }
+            else{
+                var inpL = "It doesn't have a Login input.";
+            }
+        } 
+        else{
+            var inpL = "It doesn't have a Login input.";
         }
     } 
     else{         
     };
+    var myAnchorObjet = document.getElementById("anchor-register");
+    var anchorHref = myAnchorObjet.href;
+    var compAnchor ="file:///D:/Curso%20Radium%20Rocket/Wek-9/register.html";
+    if(anchorHref == compAnchor){
+        var anchT ="It has a correct Anchor Tag.";
+    }
+    else{
+        var anchT ="It doesn't have a correct Anchor Tag.";
+    }
 document.getElementById("validation").style.display = "block";
 document.getElementById("validation").innerHTML = "<p>"+ compE + "</p>" +
-"<p>" + compP + "</p>" +"<p>"+ inpE + "</p>" +
-"<p>" + inpP + "</p>";
+ "<p>"+ inpE + "</p>" + "<p>" + inpER + "</p>" + "<p>" + compP + "</p>" +
+"<p>" + inpP + "<p>" + inpPR + "</p>" + "</p>" + "<p>" + inpL + "</p>"+"<p>" + anchT + "</p>";
 }
